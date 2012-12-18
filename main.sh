04d2b6fd26ce38c04d6378949a6caf6be0239219
cordovatj <cordovatj@basedatos00.(none)>
2012-12-18 11:02:11 -0430

    Corrección al abrir capítulo en una línea específica, y apertura con nohup

--- main.sh
+++ main.sh
@@ -5,9 +5,30 @@
 RUTA=`dirname $0`
 
 CAPITULO=`echo $1" "$2 | egrep '[0-9]+ [0-9]+' | wc -l`
+LINEA=`echo $1" "$2" "$3 | egrep '[0-9]+ [0-9]+ [0-9]+' | wc -l`
 
-if [ $CAPITULO -eq 1 ]; then
+if [ $LINEA -eq 1 ]; then
 
+    #echo "linea"
+    TOMO=$1
+    CAP=$2
+    NUM_LIN=$3
+
+    if [ $CAP -lt 10 ]; then
+	
+	CAP=`echo "0${CAP}"`
+
+    fi  
+
+    if [ -f ${RUTA}/cap/T${TOMO}C${CAP} ];then
+       nohup gedit ${RUTA}/cap/T${TOMO}C${CAP} +$NUM_LIN &
+    else
+	echo "No existe el capítulo T${TOMO}C${CAP}"
+    fi
+
+elif [ $CAPITULO -eq 1 ]; then 
+
+    #echo "capitulo"
     TOMO=$1
     CAP=$2
 
@@ -18,12 +39,11 @@ if [ $CAPITULO -eq 1 ]; then
     fi
 	
     if [ -f ${RUTA}/cap/T${TOMO}C${CAP} ];then
-       gnome-open ${RUTA}/cap/T${TOMO}C${CAP}
+      nohup gedit ${RUTA}/cap/T${TOMO}C${CAP} &
     else
 	echo "No existe el capítulo T${TOMO}C${CAP}"
     fi
 
-    
 else
 	cd $RUTA
 	#grep --color=auto -niA2 -B2 $1 ${RUTA}/qui*
